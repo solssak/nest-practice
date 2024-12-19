432703ece02fab05a2ebe4ee6de574544d1bb2a2
solssak <gswoong1012@gmail.com>
2024-12-20 05:09:58 +0900

    chore: link DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import * as Joi from '@hapi/joi';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        // environment variables type validation
+        BACKEND_PORT: Joi.number().required(),
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+    BoardsModule,
+  ],
 })
 export class AppModule {}
