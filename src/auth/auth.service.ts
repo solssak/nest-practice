76bb9861e69b8efba4052ed9b70f1fdebcd76b85
solssak <gswoong1012@gmail.com>
2024-12-24 21:39:07 +0900

    feat: Add signUp

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialDto): Promise<User> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.userRepository.create({ username, password });
+
+    await this.userRepository.save(user);
+
+    return user;
+  }
+}
